3cdcfab06d62aab87460483bbf28cfc439ddb446
ajin <ajinalex05@gmail.com>
2022-09-19 19:12:18 +0530

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Yes im in... yahooo");
+        for(int i=0;i<100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
